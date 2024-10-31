cdc8db9d01a79f26d0073db689dcb6c269690a78
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2024-11-01 00:57:32 +0530

    Tape section completed

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from "@/assets/icons/star.svg";
+import { Fragment } from "react";
 const words = [
   "Performant",
   "Accessible",
@@ -14,18 +15,27 @@ const words = [
 ];
 export const TapeSection = () => {
   return (
-    <div className="py-16 lg:py-20 overflow-clip">
+    <div className="py-16 lg:py-20 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map((word) => {
+          <div className="flex flex-none gap-4 py-3 animate-move-left [animation-duration:30s] pr-4">
+            {[...new Array(2)].map((_, idx) => {
               return (
-                <div className="inline-flex gap-4 items-center" key={word}>
-                  <span className="text-gray-900 font-extrabold uppercase text-sm">
-                    {word}
-                  </span>
-                  <StarIcon className="size-6 text-gray-900 -rotate-12" />
-                </div>
+                <Fragment key={idx}>
+                  {words.map((word) => {
+                    return (
+                      <div
+                        className="inline-flex gap-4 items-center"
+                        key={word}
+                      >
+                        <span className="text-gray-900 font-extrabold uppercase text-sm">
+                          {word}
+                        </span>
+                        <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                      </div>
+                    );
+                  })}
+                </Fragment>
               );
             })}
           </div>
